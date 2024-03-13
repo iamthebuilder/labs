1723f23fd1aa2a06ea68356c1bd80cca023e7bd4
Alexandr <159799314+iamthebuilder@users.noreply.github.com>
2024-03-13 18:27:24 +0300

    Add files via upload

---
+++ threeone/threeone/Header.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+//void create_stream(ofstream& outfile,int n);
+
+void merge(ofstream& result, ifstream& first_file, ifstream& second_file);
+
